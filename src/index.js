b1b3439d7ba5d16c8133f7298dcbf34f8ed1b1ab
Mic-Tyson <mkooner@live.com>
2024-11-26 18:46:32 +0000

    add example usage

--- src/index.js
+++ src/index.js
@@ -1,5 +1,19 @@
-// index.js
-import "./styles.css";
-import { greeting } from "./greeting.js";
+import ("./styles.css")
+import { dropdownMenu } from "./dropdownMenu.js";
 
-console.log(greeting);
+// example menus
+
+const dropdown1 = dropdownMenu(
+  "Options 1",
+  { label: "Button A", callback: () => console.log("Button A clicked") },
+  { label: "Button B", callback: () => console.log("Button B clicked") }
+);
+
+const dropdown2 = dropdownMenu(
+  "Options 2",
+  { label: "Button X", callback: () => console.log("Button X clicked") },
+  { label: "Button Y", callback: () => console.log("Button Y clicked") }
+);
+
+document.body.appendChild(dropdown1);
+document.body.appendChild(dropdown2);
